72dd1c96c622196aefcb2378271dbcdb5741aa69
nardtree <gim.kobayashi@gmail.com>
2019-07-23 07:12:57 +0000

    improve to handle np.nan object of CountEncoder

--- CountEncoder.py
+++ CountEncoder.py
@@ -3,11 +3,11 @@ import pandas as pd
 import numpy as np
 from collections import Counter
 
+
 def encode_with_columns_mutable(input_dfs=[], columns=[]):
-    df = pd.concat(input_dfs)
+    dfConc = pd.concat(input_dfs)
 
     for column in columns:
-        feat_freq = dict(Counter(df[column].tolist()))
-        for adf in input_dfs:
-            adf[f'{column}/CountEncode'] = adf[column].apply(lambda x:feat_freq[x])
-        #print(column, feat_freq)
+        feat_freq = dict(Counter(dfConc[column].fillna("None").tolist()))
+        for df in input_dfs:
+            df[f'CountEncode_{column}'] = df[column].fillna("None").apply(lambda x: feat_freq[x])
