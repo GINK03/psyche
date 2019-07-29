65f93ccfe350632943320c6a886d741301d02b2b
gimpei <gim.kobayashi@gmail.com>
2019-07-29 18:32:23 +0900

    temporary update

--- PytorchDataloaderUnet.py
+++ PytorchDataloaderUnet.py
@@ -12,6 +12,7 @@ import os
 
 # This is base on https://www.kaggle.com/witwitchayakarn/u-net-with-pytorch
 
+
 def rle_decode(mask_rle, shape=(768, 768)):
     '''
     mask_rle: run-length as string formated (start length)
@@ -28,6 +29,7 @@ def rle_decode(mask_rle, shape=(768, 768)):
         im[lo:hi] = 1
     return im.reshape(shape).T
 
+
 def rle_encode(im):
     '''
     im: numpy array, 1 - mask, 0 - background
@@ -40,90 +42,74 @@ def rle_encode(im):
     runs[::2] -= 1
     return ' '.join(str(x) for x in runs)
 
+
 class ImgMaskUnetDataset(Dataset):
     def __init__(self,
-                 img_dpath,
                  img_fnames,
                  img_transform,
-                 mask_encodings=None,
-                 mask_size=None,
-                 mask_transform=None):
-        self.img_dpath = img_dpath
+                 injection=None,
+                 mask_transform=None
+                 ):
         self.img_fnames = img_fnames
         self.img_transform = img_transform
-        self.mask_encodings = mask_encodings
-        self.mask_size = mask_size
+        self.injection = injection
         self.mask_transform = mask_transform
 
     def __getitem__(self, i):
-        # https://github.com/pytorch/vision/issues/9#issuecomment-304224800
-        seed = np.random.randint(2147483647)
-        fname = self.img_fnames[i]
-        fpath = os.path.join(self.img_dpath, fname)
-        img = Image.open(fpath)
-        if self.img_transform is not None:
-            random.seed(seed)
-            img = self.img_transform(img)
-        if self.mask_encodings is None:
-            return img, fname
-        if self.mask_size is None or self.mask_transform is None:
+        img = Image.open(self.injection.get_path(self.img_fnames[i]))
+        #print(self.mask_transform)
+        if self.mask_transform is None:
             raise ValueError(
                 'If mask_dpath is not None, mask_size and mask_transform must not be None.')
-        mask = np.zeros(self.mask_size, dtype=np.uint8)
-        # NaN doesn't equal to itself
-        if self.mask_encodings[fname][0] == self.mask_encodings[fname][0]:
-            for encoding in self.mask_encodings[fname]:
-                mask += rle_decode(encoding, self.mask_size)
-        mask = np.clip(mask, 0, 1)
-        mask = Image.fromarray(mask)
-        random.seed(seed)
-        mask = self.mask_transform(mask)
-        return img, torch.from_numpy(np.array(mask, dtype=np.int64))
+        # このへんでinjection patternを使う
+        masks = self.injection.get_mask(self.img_fnames[i], np.array(img))
+        #masks = self.mask_transform(mask)
+        return torch.from_numpy(np.array(img).reshape(3, 256, 1600).astype(np.float)).float(), torch.from_numpy(np.array(masks, dtype=np.int64))
 
     def __len__(self):
         return len(self.img_fnames)
 
 
-def get_each_unet_dataloader(channel_means, channel_stds, img_size, batch_size, num_workers,
-                             original_img_size,
-                             train_dpath, train_fnames, train_encodings,
-                             val_dpath, val_fnames, val_encodings,
-                             test_dpath, test_fnames):
+def get_each_unet_dataloader(img_size,
+                             batch_size,
+                             num_workers,
+                             injection_train_val,
+                             train_fnames,
+                             val_fnames,
+                             test_fnames):
     train_transform = transforms.Compose([transforms.Resize(img_size),
                                           transforms.RandomRotation(360),
-                                          transforms.ToTensor(),
-                                          transforms.Normalize(channel_means, channel_stds)])
+                                          transforms.ToTensor(), ])
+    # transforms.Normalize(channel_means, channel_stds)])
     val_transform = transforms.Compose([transforms.Resize(img_size),
-                                        transforms.ToTensor(),
-                                        transforms.Normalize(channel_means, channel_stds)])
+                                        transforms.ToTensor(), ])
+    # transforms.Normalize(channel_means, channel_stds)])
     mask_transform = transforms.Compose([transforms.Resize(img_size),
                                          transforms.RandomRotation(360)])
 
-    train_dataloader = DataLoader(ImgMaskUnetDataset(train_dpath,
-                                                     train_fnames,
-                                                     train_transform,
-                                                     train_encodings,
-                                                     original_img_size,
-                                                     mask_transform),
-                                  batch_size=batch_size,
-                                  shuffle=True,
-                                  pin_memory=torch.cuda.is_available(),
-                                  num_workers=num_workers)
-    val_dataloader = DataLoader(ImgMaskUnetDataset(val_dpath,
-                                                   val_fnames,
-                                                   val_transform,
-                                                   val_encodings,
-                                                   original_img_size,
-                                                   mask_transform),
-                                batch_size=batch_size,
-                                shuffle=False,
-                                pin_memory=torch.cuda.is_available(),
-                                num_workers=num_workers)
-    test_dataloader = DataLoader(ImgMaskUnetDataset(test_dpath,
-                                                    test_fnames,
-                                                    val_transform),
-                                 batch_size=batch_size,
-                                 shuffle=False,
-                                 pin_memory=torch.cuda.is_available(),
-                                 num_workers=num_workers)
+    train_dataloader = DataLoader(ImgMaskUnetDataset(
+        train_fnames,
+        train_transform,
+        injection_train_val,
+        mask_transform),
+        batch_size=batch_size,
+        shuffle=True,
+        pin_memory=torch.cuda.is_available(),
+        num_workers=num_workers)
+    val_dataloader = DataLoader(ImgMaskUnetDataset(
+        val_fnames,
+        val_transform,
+        injection_train_val,
+        mask_transform),
+        batch_size=batch_size,
+        shuffle=False,
+        pin_memory=torch.cuda.is_available(),
+        num_workers=num_workers)
+    test_dataloader = DataLoader(ImgMaskUnetDataset(
+        test_fnames,
+        val_transform),
+        batch_size=batch_size,
+        shuffle=False,
+        pin_memory=torch.cuda.is_available(),
+        num_workers=num_workers)
     return (train_dataloader, val_dataloader, test_dataloader)
