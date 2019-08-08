a0c308134edf20dbfea16ff540fb3945c5c9cb42
nardtree <gim.kobayashi@gmail.com>
2019-08-08 05:39:02 +0000

    update

--- ExpectIsCategorical.py
+++ ExpectIsCategorical.py
@@ -1,6 +1,7 @@
 
 import pandas as pd
 
+
 def expect(input_dfs=[]):
     assert len(input_dfs) == 2, 'input_dfs size must be 2.'
     dfConcat = pd.concat(input_dfs)
@@ -10,5 +11,18 @@ def expect(input_dfs=[]):
         #print(column, str(dfConcat.dtypes))
         if str(dfConcat[column].dtypes) == 'object':
             #print(column, dfConcat[column].dtypes)
+            categories.append(column)
+    return categories
+
+
+def expect_by_int(input_dfs=[]):
+    assert len(input_dfs) == 2, 'input_dfs size must be 2.'
+    dfConcat = pd.concat(input_dfs)
+
+    categories = []
+    for column in dfConcat.columns:
+        #print(column, str(dfConcat.dtypes))
+        if 'int' in str(dfConcat[column].dtypes):
+            print(column, dfConcat[column].dtypes)
             categories.append(column)
     return categories
