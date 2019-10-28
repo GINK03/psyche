959631ba5c33b7839bb47bb60c3df53d229c4204
Your Name <gim.kobayashi@gmail.com>
2019-10-28 13:54:13 +0900

    add PipSetup

---
+++ PipSetup.sh
@@ -0,0 +1,14 @@
+pip install python-opencv
+pip install opencv-python
+pip install optuna
+pip install neovim
+pip install torch torchvision
+pip install lightgbm
+pip install xgboost
+pip install catboost
+pip install pandas-profiling
+pip install tensorflow-gpu
+pip install keras
+pip install networkx
+pip install altair
+pip install tqdm
